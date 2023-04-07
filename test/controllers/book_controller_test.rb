66cc7331cbeab5cdf1b9192b29f6fe13f47c1bcd
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-04-07 00:52:54 +0000

    Initial Commit2

---
+++ test/controllers/book_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class BookControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get book_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get book_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get book_show_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get book_edit_url
+    assert_response :success
+  end
+end
