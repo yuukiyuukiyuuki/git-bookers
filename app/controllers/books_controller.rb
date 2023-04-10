aff40bdd7524967e155020b5b7a01697e5d56713
yuukiyuukiyuuki <128203554+yuukiyuukiyuuki@users.noreply.github.com>
2023-04-10 14:33:09 +0000

    Initial Commit4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
   def new
+    @book=Book.new
+  end
+
+  def create
+    book=Book.new(book_params)
+    book.save
+    redirect_to'/books/creates'
   end
 
   def index
+    @books=Book.all
+    @book=Book.new
   end
 
   def show
+
   end
 
   def edit
+    @book=Book.all
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 
 resources :books
 get '/' => 'homes#top'
+get 'books'=>'books#index'
 
 end
